81b0b375a60c7d8aacccc1c2d0e082d689fd97e9
taptue kuate arnaud franck <88408395+taptue-devopsenginner@users.noreply.github.com>
2024-11-11 12:51:10 -0600

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 Name
+    bucket = "tekua-bucket" # Replace with your actual S3 Name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
